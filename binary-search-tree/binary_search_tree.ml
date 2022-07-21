d54b40d19611f77d596748a7d392c7f6b24b115c
Hoseong Son <hs.son@greenlabs.co.kr>
2022-07-21 12:59:18 +0900

    formatting code

--- binary-search-tree/binary_search_tree.ml
+++ binary-search-tree/binary_search_tree.ml
@@ -1,16 +1,11 @@
 open Base
 
-type bst
+type bst = Empty
+type node = { left : node; right : node; value : string }
 
-let empty = failwith "'empty' is missing"
-
+let empty = Empty
 let value _ = failwith "'value' is missing"
-
 let left _ = failwith "'left' is missing"
-
 let right _ = failwith "'right' is missing"
-
 let insert _ _ = failwith "'insert' is missing"
-
 let to_list _ = failwith "'to_list' is missing"
-
