1c56d51669a4944cee7c0608ed1552396b5908af
Hoseong Son <hs.son@greenlabs.co.kr>
2022-07-25 10:35:13 +0900

    impl. bst

--- binary-search-tree/binary_search_tree.ml
+++ binary-search-tree/binary_search_tree.ml
@@ -1,11 +1,35 @@
 open Base
 
-type bst = Empty
-type node = { left : node; right : node; value : string }
+type bst = Empty | Node of { left : bst; right : bst; value : int }
 
 let empty = Empty
-let value _ = failwith "'value' is missing"
-let left _ = failwith "'left' is missing"
-let right _ = failwith "'right' is missing"
-let insert _ _ = failwith "'insert' is missing"
-let to_list _ = failwith "'to_list' is missing"
+
+let value = function
+  | Empty -> Result.Error "현재값 비어있음"
+  | Node { value; _ } -> Result.Ok value
+
+let left = function
+  | Empty -> Result.Error "왼쪽 비어있음"
+  | Node { left; _ } -> Result.Ok left
+
+let right = function
+  | Empty -> Result.Error "오른쪽 비어있음"
+  | Node { right; _ } -> Result.Ok right
+
+let rec insert x = function
+  (* 비어있는 트리라면 init *)
+  | Empty -> Node { value = x; left = Empty; right = Empty }
+  (* tree가 비어있지 않고 삽입하려는 값이 value보다 클 경우 오른쪽으로 insert *)
+  | Node { value; left; right } when value < x ->
+      Node { value; left; right = insert x right }
+  (* tree가 비어있지 않고 삽입하려는 값이 value보다 작을 경우 왼쪽으로 insert *)
+  | Node { value; left; right } when value >= x ->
+      Node { value; left = insert x left; right }
+  (* 이외의 케이스는 그대로 반환 *)
+  | Node {value; left; right} -> Node {value; left; right}
+
+let rec to_list = function
+  (* 트리가 비어있으면 빈 리스트 *)
+  | Empty -> []
+  (* 그렇지 않다면 left + value + right *)
+  | Node { value; left; right } -> (to_list left) @ [value] @ (to_list right)
